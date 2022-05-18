fef7a1c8638cb40e328b284f22a4728b5748126c
Rebecca Sissman <rsissman15@aol.com>
2022-05-18 13:24:34 -0400

    commit tree c6155e0ce12036e9f901e153e60415e2ba9df243

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color,:size,:material,:condition
+    def initialize(brand)
+        @brand=brand
+    end
+    def cobble(condition="new")
+        @condition=condition
+        puts "Your shoe is as good as new!"
+    end
+end
+
+shoe_one=Shoe.new("Adidas")
